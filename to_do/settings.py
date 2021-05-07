9d977e9ced47437df4f57666e64bf25d93dc0fef
mnaile <mammadaliyevan@gmail.com>
2021-05-07 12:47:40 +0400

    added user endpoint

--- to_do/settings.py
+++ to_do/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'todo_app.apps.TodoAppConfig',
     'rest_framework',
     'rest_framework.authtoken',
@@ -53,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'to_do.urls'
 
